f6765da2594aebba937137133748577e6384690e
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-02-21 15:18:34 +0100

    Cleanup CLI

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,7 @@ import figlet = require('figlet');
 import { readFileSync, writeFileSync } from 'fs';
 import yargs, { Arguments, Options } from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { FpbJsModel } from './fpb-model/fpb.interface';
+import { FpbJsModel } from './fpb.interface';
 import { map } from './mapper';
 
 
@@ -17,7 +17,7 @@ function handler (argv: Arguments<Options>): void {
     process.exit(0);
 }
 
-const argv = yargs(process.argv.slice(2))
+const argv = yargs(hideBin(process.argv))
     .usage(header + "\n\n")
     .usage('Usage: $0 map <filePath> [string] -n [string]')
     .command({
@@ -43,40 +43,3 @@ const argv = yargs(process.argv.slice(2))
     .help()
     .epilog(" \n")
     .argv;
-
-
-
-
-// export const builder: CommandBuilder<Options, Options> = (yargs) =>
-//     yargs
-//         .usage('Usage: $0 map <fileName> [string] -ns [string]')
-//         .command("map", "Map fpbjs export from given <fileName> into FPB ontology inside")
-//         .positional('fileName', { 
-//             describe: "Name of the file that is going to be mapped",
-//             type: 'string',
-//             demandOption: true
-//         })
-//         .options({
-//             ns: {
-//                 alias: "namespace",
-//                 describe: "The namespace that all individuals are mapped into",
-//                 type: 'string',
-//             },
-//         });
-
-// export const handler = (argv: Arguments<Options>): void => {
-//     console.log("handler called");
-//     console.log(argv);
-	
-//     const { fileName, ns } = argv;
-//     const fileContent = fs.readFileSync(fileName as string, 'utf-8').toString();
-//     console.log("file");
-	
-//     console.log(fileContent);
-	
-//     const fpbModel = JSON.parse(fileContent) as FpbJsModel;
-//     // const model = JSON.parse(fileName as string) as FpbJsModel;
-//     const mappedFpbFile = map(fpbModel, ns as string);
-//     fs.writeFileSync("./mapped-ontology.ttl", mappedFpbFile);
-//     process.exit(0);
-// };
