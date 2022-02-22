cc5869d8ff050d50b51d2a84f1578789c1ce8a3a
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-02-22 17:00:23 +0100

    Added ontologyIri option

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,28 @@
-// import * as figlet from "figlet";
-const font = import("figlet/importable-fonts");
+// The following two lines are a horribly bad way to include the figlet font but the only one that worked 
+// eslint-disable-next-line
+// @ts-ignore
+import standard from 'figlet/importable-fonts/Standard';
+import * as figlet from "figlet";
 import { readFileSync, writeFileSync } from 'fs';
 import yargs, { Arguments, Options } from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { FpbJsModel } from './fpb.interface';
 import { map } from './mapper';
 
-
-// const header = figlet.textSync('fpb.js to ODP', { horizontalLayout: 'full' });
+figlet.parseFont('Standard', standard);
+const header = figlet.textSync('fpb.js to ODP', { horizontalLayout: 'full', font: 'Standard'});
 
 function handler (argv: Arguments<Options>): void {
-    const { filePath, ns } = argv;
+    const { filePath, n, o } = argv;
     const fileContent = readFileSync(filePath as string, 'utf-8').toString();
     const fpbModel = JSON.parse(fileContent) as FpbJsModel;
-    const mappedFpbFile = map(fpbModel, ns as string);
+    const mappedFpbFile = map(fpbModel, n as string, o as string);
     writeFileSync("./mapped-ontology.ttl", mappedFpbFile);
     process.exit(0);
 }
 
 const argv = yargs(hideBin(process.argv))
-    // .usage(header + "\n\n")
+    .usage(header + "\n\n")
     .usage('Usage: $0 map <filePath> [string] -n [string]')
     .command({
         command: 'map <filePath>', 
@@ -30,7 +33,7 @@ const argv = yargs(hideBin(process.argv))
         type: "string",
         describe: "Path to the model exported from fpb.js that should be mapped to an FPB ontology"
     })
-    .demandOption("filePath", "wtf")
+    .demandOption("filePath", "File path is missing. Please provide a filepath after the 'map' command.")
     .demandCommand()
     .option("n", {
         alias: "namespace",
@@ -38,8 +41,14 @@ const argv = yargs(hideBin(process.argv))
         type: "string",
         nargs: 1,
     })
+    .option("o", {
+        alias: "ontologyIri",
+        describe: "IRI of the ontology that will be created",
+        type: "string",
+        nargs: 1,
+    })
     .hide("version")
-    .hide("map")
+    .alias("h", "help")
     .wrap(null)
     .help()
     .epilog(" \n")
