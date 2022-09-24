bac52702fbd8d27f323da25e96e302d76e2430aa
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-09-24 13:16:49 +0200

    Added test with custom namespace and ontologyIri

--- tests/mapper.test.ts
+++ tests/mapper.test.ts
@@ -13,6 +13,17 @@ describe('Mapper test', () => {
         assert.deepEqual(result, expectedResult, 'Expected result should have all values extracted...');
     });
 
+    it('Should map a given fpb.js model with a specific namespace and ontologyIri', async () => {
+        const input = JSON.parse(readFileSync("tests/fpbjs-models/simpleTwoStageProcess.json").toString()) as FpbJsModel;
+        const expectedResult = readFileSync("tests/mappedOntologies/simpleTwoStageProcessWithNamespace.ttl").toString();
+
+        const namespace ="http://www.just-a-test.com/ontologies#";
+        const ontologyIri ="http://www.just-a-test.com/ontologies/test-onto";
+
+        const result = map(input,namespace, ontologyIri);	// No namespace and ontologyIri given -> should use default
+        assert.deepEqual(result, expectedResult, 'Expected result should have all values extracted...');
+    });
+
 
     it('Should map a very simple process with one operator without graphics information', async () => {
         const input = JSON.parse(readFileSync("tests/fpbjs-models/simpleProcessWithoutGraphics.json").toString()) as FpbJsModel;
